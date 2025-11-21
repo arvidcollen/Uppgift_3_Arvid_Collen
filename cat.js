9c0e5c0c024fd908666a8515853cda3e1824ec20
arvidcollen <arvid.collen@gritacademy.se>
2025-11-21 16:02:13 +0100

    Update cat.js

--- cat.js
+++ cat.js
@@ -1,6 +1,13 @@
-//Schrödingers cat, lyckades inte få 50-50 chansen
-let cat = {alive:true, name:"Gustav"};
+//Schrödingers cat, lyckades inte få 50-50 chansen när jag skrev den
+
+/*let cat = {alive:true, name:"Gustav"};
 
 function showcat(){
     
+}*/
+
+let cat = {
+alive: Math.random( ) < 0.5 ? " är levande" : " är död!!!" ,
+name:"Shady"
 }
+alert(  "Katten heter: "+cat.name  + " och"+ cat.alive)
